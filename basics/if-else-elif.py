e0e4aa25624f4f2846b26d6c0de6383e160bb745
Aarush Maurya <238880824+aarushmaurya-work@users.noreply.github.com>
2026-02-07 11:33:39 +0530

    Added if-else-elif.py
    this shows how to set conditions in python

--- basics/if-else-elif.py
+++ basics/if-else-elif.py
@@ -1,1 +1,10 @@
 #if is use to set conditions
+
+age = int(input(f"Enter your age : "))
+if age < 1 :
+    print("Invalid Age") #if is independent, and the first condition check
+elif age < 18 :
+    print("Access Denied") #elifs are the check after the if is False, all the Elifes are checked one by one as per the order
+else: 
+    print("Welcome!")#if every condition check is Flase, then the else block is executed
+
